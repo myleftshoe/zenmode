190d689631842e44d6c1a2e0142ac66454525bac
myleftshoe <paulesammut@gmail.com>
2021-02-06 19:36:45 +1100

    stageframe.js

--- stage.js
+++ stage.js
@@ -1,17 +1,12 @@
 const { GObject, Clutter, St } = imports.gi
 const Extension = imports.misc.extensionUtils.getCurrentExtension()
-const Log = Extension.imports.logger
-const { ll } = Log
 const { merge } = Extension.imports.object
 const { Pane } = Extension.imports.pane
+const { StageFrame } = Extension.imports.stageframe
 const { layouts } = Extension.imports.layouts
-const { Chrome } = Extension.imports.chrome
 const { sampleColors } = Extension.imports.metaWindow
-
-const primaryMonitor = global.display.get_current_monitor()
-const monitor = global.display.get_monitor_geometry(primaryMonitor)
-
-const spacing = 40
+const Log = Extension.imports.logger
+const { ll } = Log
 
 
 var createStage = (props) => new Stage(props)
@@ -40,7 +35,7 @@ var Stage = GObject.registerClass(
                 reactive: false,
                 vertical: false,
             })
-            this.frame = new StageFrame(spacing)
+            this.frame = new StageFrame()
             global.stage.add_child(this)
             this.setLayout(layout)
         }
@@ -113,42 +108,4 @@ function get_all_descendants(actor) {
     }
     getChildren(actor)
     return children
-}
-
-
-class StageFrame {
-    constructor(vert = spacing, horz = vert) {
-        this.top = new Chrome({reactive: true})
-        this.bottom = new Chrome()
-        this.left = new Chrome({})
-        this.right = new Chrome()
-        this.setVert(vert)
-        this.setHorz(horz)
-    }
-    show() {
-        this.top.show()
-        this.bottom.show()
-        this.left.show()
-        this.right.show()
-    }
-    hide() {
-        this.top.hide()
-        this.bottom.hide()
-        this.left.hide()
-        this.right.hide()
-    }
-    setVert(size) {
-        this.top.width = monitor.width
-        this.top.height = size
-        this.bottom.y = monitor.height - size
-        this.bottom.width = monitor.width
-        this.bottom.height = size
-    }
-    setHorz(size) {
-        this.left.width = size;
-        this.left.height = monitor.height
-        this.right.x = monitor.width - size
-        this.right.width = size
-        this.right.height = monitor.height
-    }
 }
---
+++ stageframe.js
@@ -0,0 +1,44 @@
+const Extension = imports.misc.extensionUtils.getCurrentExtension()
+const { Chrome } = Extension.imports.chrome
+
+const primaryMonitor = global.display.get_current_monitor()
+const monitor = global.display.get_monitor_geometry(primaryMonitor)
+
+const spacing = 40
+
+var StageFrame = class StageFrame {
+    constructor(vert = spacing, horz = vert) {
+        this.top = new Chrome({reactive: true})
+        this.bottom = new Chrome()
+        this.left = new Chrome({})
+        this.right = new Chrome()
+        this.setVert(vert)
+        this.setHorz(horz)
+    }
+    show() {
+        this.top.show()
+        this.bottom.show()
+        this.left.show()
+        this.right.show()
+    }
+    hide() {
+        this.top.hide()
+        this.bottom.hide()
+        this.left.hide()
+        this.right.hide()
+    }
+    setVert(size) {
+        this.top.width = monitor.width
+        this.top.height = size
+        this.bottom.y = monitor.height - size
+        this.bottom.width = monitor.width
+        this.bottom.height = size
+    }
+    setHorz(size) {
+        this.left.width = size;
+        this.left.height = monitor.height
+        this.right.x = monitor.width - size
+        this.right.width = size
+        this.right.height = monitor.height
+    }
+}
